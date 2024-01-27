3214e2cf0a0dc2b12b0b611e22f34066197eb816
FroApril <188494506@qq.com>
2024-01-27 21:31:54 +1100

    home page development finished

---
+++ client/blog-app/src/App.tsx
@@ -0,0 +1,37 @@
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
+    </div>
+  );
+};
+
+export default App;
